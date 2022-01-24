ba283c1523b85a66fddca78112dcb6dd0ac9b712
Nader Nasr <nadernasr7@gmail.com>
2022-01-24 17:20:29 -0500

    generate random string on submit

--- express_server.js
+++ express_server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const generateRandomString = () => {
+  return (Math.random() + 1).toString(36).substring(7);
 };
 
 //landing page route.
@@ -23,6 +29,12 @@ app.get("/urls", (req, res) => {
 //Form route
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+//Post request to create a new tiny url.
+app.post("/urls", (req, res) => {
+  console.log(req.params);
+  res.send(generateRandomString());
 });
 
 //Second route to render short and long URL.
